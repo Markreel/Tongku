aa4a14a735bc09b4d5d51b8e12aa5d576a25c332
Markreel <markmeerssman@live.nl>
2020-10-31 14:18:18 +0100

    Transfered the entire project to the repository

---
+++ Environmental Modeling/Assets/Scripts/Gate.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    [SerializeField] float openGateDuration = 3f;
+    [SerializeField] float closeGateDuration = 0.5f;
+    [SerializeField] AnimationCurve openGateCurve;
+    [SerializeField] AnimationCurve closeGateCurve;
+    [SerializeField] Vector3 openGateOffset;
+    private Coroutine openGateRoutine;
+
+    [SerializeField] AudioClip openGateSFX;
+    [SerializeField] AudioClip closeGateSFX;
+    [SerializeField] AudioClip shutGateSFX;
+    private AudioSource audioSource;
+
+    private Vector3 originalPos;
+
+    void Start()
+    {
+        originalPos = transform.position;
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    public void OpenGate()
+    {
+        if (openGateRoutine != null) StopCoroutine(openGateRoutine);
+        openGateRoutine = StartCoroutine(IOpenGate());
+    }
+
+    public void CloseGate()
+    {
+        if (openGateRoutine != null) StopCoroutine(openGateRoutine);
+        openGateRoutine = StartCoroutine(ICloseGate());
+    }
+
+    IEnumerator IOpenGate()
+    {
+        Vector3 _startPos = transform.position;
+        audioSource.PlayOneShot(openGateSFX);
+        float _loopTimer = openGateSFX.length;
+
+        float _lerpTime = 0;
+        while (_lerpTime < 1)
+        {
+            _lerpTime += Time.deltaTime / openGateDuration;
+            float _lerpKey = openGateCurve.Evaluate(_lerpTime);
+
+            _loopTimer -= Time.deltaTime;
+            if(_loopTimer <= 0)
+            {
+                _loopTimer = openGateSFX.length;
+                audioSource.PlayOneShot(openGateSFX);
+            }
+
+
+            transform.position = Vector3.Lerp(_startPos, originalPos + openGateOffset, _lerpKey);
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+    IEnumerator ICloseGate()
+    {
+        Vector3 _startPos = transform.position;
+        audioSource.PlayOneShot(closeGateSFX);
+        float _loopTimer = openGateSFX.length;
+
+        float _lerpTime = 0;
+        while (_lerpTime < 1)
+        {
+            _lerpTime += Time.deltaTime / closeGateDuration;
+            float _lerpKey = closeGateCurve.Evaluate(_lerpTime);
+
+            _loopTimer -= Time.deltaTime;
+            if (_loopTimer <= 0)
+            {
+                _loopTimer = openGateSFX.length;
+                audioSource.PlayOneShot(openGateSFX);
+            }
+
+            transform.position = Vector3.Lerp(_startPos, originalPos, _lerpKey);
+
+            yield return null;
+        }
+
+        audioSource.PlayOneShot(shutGateSFX);
+
+        yield return null;
+    }
+
+
+}
---
+++ Environmental Modeling/Assets/Scripts/PressurePlate.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour
+{
+    [SerializeField] Gate gate;
+    [Space]
+    [SerializeField] float pressPlateDuration = 0.5f;
+    [SerializeField] AnimationCurve pressPlateCurve;
+    [SerializeField] Vector3 pressedOffset;
+    private Coroutine pressPlateRoutine;
+
+    [SerializeField] AudioClip pressedSFX;
+    [SerializeField] AudioClip releasedSFX;
+    private AudioSource audioSource;
+
+    private Vector3 originalPos;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null) audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.loop = false;
+    }
+
+    void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            PressPlate();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            ReleasePlate();
+    }
+
+    void PressPlate()
+    {
+        if(pressPlateRoutine != null) StopCoroutine(pressPlateRoutine);
+        pressPlateRoutine = StartCoroutine(IPressPlate());
+    }
+    void ReleasePlate()
+    {
+        gate.CloseGate();
+        audioSource.PlayOneShot(releasedSFX);
+        if (pressPlateRoutine != null) StopCoroutine(pressPlateRoutine);
+        pressPlateRoutine = StartCoroutine(IReleasePlate());
+    }
+
+    IEnumerator IPressPlate()
+    {
+        Vector3 _startPos = transform.position;
+
+        float _lerpTime = 0;
+        while (_lerpTime < 1)
+        {
+            _lerpTime += Time.deltaTime / pressPlateDuration;
+            float _lerpKey = pressPlateCurve.Evaluate(_lerpTime);
+
+            transform.position = Vector3.Lerp(_startPos, originalPos + pressedOffset, _lerpKey);
+
+            yield return null;
+        }
+
+        audioSource.PlayOneShot(pressedSFX);
+        gate.OpenGate();
+
+        yield return null;
+    }
+    IEnumerator IReleasePlate()
+    {
+        Vector3 _startPos = transform.position;
+
+        float _lerpTime = 0;
+        while (_lerpTime < 1)
+        {
+            _lerpTime += Time.deltaTime / pressPlateDuration;
+            float _lerpKey = pressPlateCurve.Evaluate(_lerpTime);
+
+            transform.position = Vector3.Lerp(_startPos, originalPos, _lerpKey);
+
+            yield return null;
+        }
+
+        yield return null;
+    }
+
+
+
+}
